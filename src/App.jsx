0049c1e7260ad619437a6aa23fdbcb87dff2453c
temp <maheshvengala4321@gmail.com>
2024-06-08 20:20:50 +0530

    testing stage

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import "./App.css";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./Pages/Home";
-// import Transactions from "./Pages/Transactions";
+import Home from "./pages/Home/Home";
+import Transactions from "./pages/Transactions/Transactions";
 // import Payment from "./Pages/Payment";
 // import ContactUs from "./Pages/ContactUs";
 // import PrivacyPolicy from "./Pages/PrivacyPolicy";
@@ -16,9 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          {/* <Route path="/home" element={<Home />} />
-          <Route path="/payment" element={<Payment />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/transactions" element={<Transactions />} />
+          {/* <Route path="/payment" element={<Payment />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
           <Route path="/termsandconditions" element={<TermsAndConditions />} /> */}
