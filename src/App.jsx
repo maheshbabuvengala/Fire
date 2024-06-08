a0057c071a2596c571ea07b9d8d475297b4fc65a
temp <maheshvengala4321@gmail.com>
2024-06-08 20:39:17 +0530

    testing stage 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Login from "./pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Transactions from "./pages/Transactions/Transactions";
-// import Payment from "./Pages/Payment";
-// import ContactUs from "./Pages/ContactUs";
-// import PrivacyPolicy from "./Pages/PrivacyPolicy";
-// import TermsAndConditions from "./Pages/TermsAndConditions";
+import Payment from "./pages/Payment/Payment";
+import ContactUs from "./pages/ContactUs";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
 
 function App() {
   return (
@@ -18,10 +18,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
           <Route path="/transactions" element={<Transactions />} />
-          {/* <Route path="/payment" element={<Payment />} />
+          <Route path="/payment" element={<Payment />} />
           <Route path="/contactus" element={<ContactUs />} />
           <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-          <Route path="/termsandconditions" element={<TermsAndConditions />} /> */}
+          <Route path="/termsandconditions" element={<TermsAndConditions />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Payment/Payment.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Scanner from "../../assets/Scanner.jpg";
+import "./Payment.css";
+import Paytm from "../../assets/paytm.jpg";
+import phonepe from "../../assets/phonepe.jpg";
+import googlepay from "../../assets/googlepay.jpg";
+import Navbar from "../Navbar/Navbar";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+
+const Payment = () => {
+  const [free, setfree] = useState("");
+  const [usernames, setUsernames] = useState("");
+
+  const [upiid, setupiid] = useState();
+
+  const navigate = useNavigate();
+
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState("");
+  const [freefire, setfreefire] = useState("");
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get(
+          "https://firescrimbackend.onrender.com/api/items",
+          { withCredentials: true }
+        );
+        setTransactions(response.data);
+        setUser(response.data[0].username);
+        setfreefire(response.data[0].freefireid);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching transactions:", error);
+        setError("Error fetching transactions. Please try again later.");
+        setLoading(false);
+      }
+    };
+
+    fetchTransactions();
+  }, []);
+
+  const username = user;
+  const freefireid = freefire;
+  const status = "Success";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://firescrimbackend.onrender.com/payment", {
+        username,
+        freefireid,
+        upiid,
+        status,
+      })
+      .then(navigate("/home"))
+      .catch(err)(console.log(err));
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="conatiner">
+        <br />
+        <h1 style={{ textAlign: "center" }}>Payment Page</h1>
+        <div className="container payment">
+          <div className="left">
+            <form action="" onSubmit={handleSubmit}>
+              <label htmlFor="utr">
+                <h4 className="wel">UTR id/transaction id/ref.NO :</h4>
+              </label>
+              <div className="imgs">
+                <img src={phonepe} />
+                <img src={googlepay} />
+                <img src={Paytm} />
+              </div>
+              <label htmlFor="" className="ins">
+                UTR id/transaction id/ref.NO :
+              </label>
+              <input
+                type="text"
+                name="utr"
+                id="utr"
+                className="input"
+                placeholder="enter according to your payment app"
+                onChange={(e) => setupiid(e.target.value)}
+              />
+              <br />
+              <button
+                type="submit"
+                className="btn btn-primary"
+                style={{ marginTop: "30px", marginLeft: "35%" }}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+          <div className="right">
+            <img src={Scanner} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Payment;
