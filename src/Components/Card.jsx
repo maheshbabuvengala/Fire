a6591fb2e4ab49a90e51811386b3cc96c2de4a9a
Mahesh Babu Vengala <155898279+maheshbabuvengala@users.noreply.github.com>
2024-07-09 13:43:40 +0530

    Update Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -12,8 +12,8 @@ const Card = () => {
             <h5 class="card-title">Tournament</h5>
             <p class="card-text">
               <h3>Map: Bermuda</h3>
-              <h3>date: 07/07/24</h3>
-              <h3>Time: 12:30 pm</h3>
+              <h3>date: 14/07/24</h3>
+              <h3>Time: 06:00 pm</h3>
               <h3>Type: Solo</h3>
             </p>
           </div>
@@ -31,7 +31,7 @@ const Card = () => {
           <div class="card-body">
             <button className="btn btn-md btn-danger clll" >
               <Link
-                //to={"/duopayment"}
+                to={"/duopayment"}
                 style={{ textDecoration: "none", color: "white" }}
               >
                 Register
@@ -40,49 +40,13 @@ const Card = () => {
             
           </div>
         </div>
-        {/* <div class="card" style={{ width: "23rem", margin: "10px" }}>
-          <img class="card-img-top" src={cardbg} alt="Card image cap" />
-          <div class="card-body">
-            <h5 class="card-title">Tournament</h5>
-            <p class="card-text">
-              <h3>Map: Bermuda</h3>
-              <h3>date: 30/06/24</h3>
-              <h3>Time: 06:00 pm</h3>
-              <h3>Type: Solo</h3>
-            </p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">
-              <b>Entry Fee:</b> <span>&#8377;</span> 20
-            </li>
-            <li class="list-group-item">
-              <b>Price: 1 kill =</b> <span>&#8377;</span> 10
-            </li>
-            <li class="list-group-item">
-              <b>Booyah : </b>
-              <span>&#8377;</span> 100+kills
-            </li>
-          </ul>
-          <div class="card-body">
-            <button className="btn btn-md btn-danger clll" onClick={() => {
-                alert("registrations are enable soon");
-              }}>
-              <Link
-                to={"/payment"}
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                Register
-              </Link>
-            </button>
-          </div>
-        </div> */}
         <div class="card" style={{ width: "23rem", margin: "10px" }}>
           <img class="card-img-top" src={cardbg} alt="Card image cap" />
           <div class="card-body">
             <h5 class="card-title">Tournament</h5>
             <p class="card-text">
               <h3>Map: Bermuda</h3>
-              <h3>date: 07/07/24</h3>
+              <h3>date: 14/07/24</h3>
               <h3>Time: 06:00 pm</h3>
               <h3>Type: Solo</h3>
             </p>
@@ -126,17 +90,6 @@ const Card = () => {
             <Link className="btn btn-primary">Register Now</Link> {/* Use to instead of href for react-router-dom */}
           </div>
         </div>
-        {/* <div className="cardss">
-          <div className="card-body">
-            <p className="card-title h4" style={{ color: 'white' }}>Map: Bermuda</p>
-            <p className="card-title h4" style={{ color: 'white' }}>Type : Squad</p>
-            <p className="card-title h4" style={{ color: 'white' }}>Time : 08:00pm</p>
-            <p className="card-title h4" style={{ color: 'white' }}>Date : 04/07/24</p>
-            <p className="card-title h4" style={{ color: 'white' }}>Entry Fee: 120</p>
-            {/* <p className="card-text">With supporting text below as a natural lead-in to additional content.</p> */}
-            {/*<Link to="paymentsquad" className="btn btn-primary">Register Now</Link> {/* Use to instead of href for react-router-dom */}
-          {/* </div>
-        </div> */} 
         <div className="cardss">
           <div className="card-body">
             <p className="card-title h4" style={{ color: 'white' }}>Map: Bermuda</p>
